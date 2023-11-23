639fee99525e81807cd6108c130e567fcdfc0354
Nikita Orliak <nikitonorl@gmail.com>
2023-11-23 19:05:27 +0100

    make build

---
+++ index.b5e3fa62.js
@@ -0,0 +1,4 @@
+// write your code here
+var e=document.querySelectorAll(".population"),n=document.querySelector(".total-population"),r=document.querySelector(".average-population"),o=[];e.forEach(function(e){return o.push(+e.innerHTML.replaceAll(",",""))});var t=o.reduce(function(e,n){return e+n},0),l=t/o.length;n.innerHTML=t.toLocaleString("en-US"),r.innerHTML=l.toLocaleString("en-US");//# sourceMappingURL=index.b5e3fa62.js.map
+
+//# sourceMappingURL=index.b5e3fa62.js.map
